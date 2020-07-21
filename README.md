7d7f51ce21ccdc32e9b026395705c84830e18770
ckeivin <chengkeivin91@gmail.com>
2020-07-21 15:41:24 +0800

    working version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # aws_ami_cleanup
+
+## Purpose
+This script cleans up AMI images based on 
+- region
+- tag key/value pair 
+- age of AMI created
+
+
+## Scripts
+
+`ami_cleanup.py` - for client machine usage
+
+`lambda_ami_cleanup.py` - formatted for lambda usage; to be accompanied with **environment variables**
+
+### Lambda Usage
+
+key|value|Description
+-|-|-|
+TAG_KEY|name|Name of the tag key
+TAG_VALUE|windows2016-base-|Name of the tag value. Script checks the value that begins with this string 
+DAYS_OLD|14|Number of days since the creation of the AMI 
---
+++ lambda_ami_cleanup.py
@@ -0,0 +1,76 @@
+import os
+import boto3
+import string
+from datetime import datetime, timedelta 
+
+def lambda_handler(event, context):
+    '''
+    variables
+    '''
+    var_region_name = os.environ['AWS_REGION']
+    tag_key = os.environ['TAG_KEY']
+    tag_val = os.environ['TAG_VALUE']
+    days_old = int(os.environ['DAYS_OLD'])
+
+    '''
+    initiate boto3 package
+    '''
+    ec2 = boto3.resource("ec2", region_name=var_region_name) 
+
+    '''
+    Filter images based on "self" and tags defined
+    '''
+    self_images = ec2.images.filter(Owners=['self'])
+    filtered_images = ec2.images.filter(
+        Owners=['self'],
+        Filters=[
+            {
+                'Name': 'tag:'+tag_key,
+                'Values': [tag_val+'*']
+                }
+        ]
+    )
+    '''
+    Print self_images
+    '''
+    print("List of self-owned AMI images are:")
+    for idx,image in enumerate(self_images):
+        print(f"[{idx}] {image.name}-{image.id}")
+    print()
+    '''
+    Print filtered_images
+    '''
+    print(f"List of matching AMI images based on tag '{tag_key}':'{tag_val}' are:")
+    for idx,image in enumerate(filtered_images):
+        print(f"[{idx}] {image.name}-{image.id}")
+    print()
+
+    '''
+    check if age of AMI images is less than "days_old"
+    '''
+    purge_images = set()
+
+    for image in filtered_images:
+        created_at = datetime.strptime(
+            image.creation_date,
+            "%Y-%m-%dT%H:%M:%S.000Z",
+        )
+        compare_date = datetime.now() - timedelta(days_old)
+        if created_at > datetime.now() - timedelta(days_old):
+            print(f"Appending to purge_images set: {image.name}[{image.id}]")
+            purge_images.add(image.id)
+    print()
+
+    '''
+    Purge AMIs
+    '''
+    print(f"Count of AMIs to purge: {len(purge_images)}")
+    if len(purge_images) > 0:
+        for image in (image for image in filtered_images if image.id in purge_images):
+            print(f"Purging: {image.name}[{image.id}]")
+            #image.deregister()  # uncomment this for actual deregistering
+            print("Purge complete!")
+    else:
+        print(f"No purging required !")
+    print()
+
